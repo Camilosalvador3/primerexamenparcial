4b4606018504ae9cdeac5bd08f570c7b82860092
Camilosalvador3 <111157715+Camilosalvador3@users.noreply.github.com>
2022-10-07 17:16:30 -0600

    Add files via upload

---
+++ ExamenPrimerParcial/PrimerExamenParcial/src/PrimerExamenParcial/CalculoDepreciacionActivos.java
@@ -0,0 +1,26 @@
+package PrimerExamenParcial;
+
+import java.util.Scanner;
+
+public class CalculoDepreciacionActivos 
+{
+ 
+	static Scanner lector = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		//Declaracion de variables
+		int metodos;
+		//Datos de entrada
+		System.out.println(" ****** BIENVENIDO AL SISTEMA ******");
+		System.out.println("Que metodo quiere seleccionar para calcular la depreciacion: ");
+		metodos = lector.nextInt();
+		System.out.println("1- Metodo De Línea Directa ");
+		System.out.println("2- Metodo De La Suma De Los Digitos Por Año ");
+		System.out.println("3- Metodo De Las Unidades Producidas ");
+		System.out.println("4- Metodo De Reduccion De Saldo ");
+		//Procesamiento	
+		//Datos de salida
+		
+	}
+
+}
